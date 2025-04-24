3eaf8402e3a94b24df7029c0c563bef99ad21f71
Gregory Becker <becker33@llnl.gov>
2025-04-24 15:50:46 -0700

    satisfies for conditional dependencies
    Signed-off-by: Gregory Becker <becker33@llnl.gov>

--- lib/spack/spack/spec.py
+++ lib/spack/spack/spec.py
@@ -3442,6 +3442,10 @@ class Spec:
         # will be verified later.
         lhs_edges: Dict[str, Set[DependencySpec]] = collections.defaultdict(set)
         for rhs_edge in other.traverse_edges(root=False, cover="edges"):
+            # Skip checking any conditional edge that is not satisfied
+            if rhs_edge.when != Spec() and not rhs_edge.parent.satisfies(rhs_edge.when):
+                continue
+
             # If we are checking for ^mpi we need to verify if there is any edge
             if spack.repo.PATH.is_virtual(rhs_edge.spec.name):
                 rhs_edge.update_virtuals(virtuals=(rhs_edge.spec.name,))
@@ -3508,10 +3512,26 @@ class Spec:
                     return False
 
         # Edges have been checked above already, hence deps=False
-        return all(
-            any(lhs.satisfies(rhs, deps=False) for lhs in self.traverse(root=False))
-            for rhs in other.traverse(root=False)
-        )
+        lhs_nodes = self.traverse(root=False)
+        for rhs in other.traverse(root=False):
+            # Possible lhs nodes to match this rhs node
+            lhss = [lhs for lhs in lhs_nodes if lhs.satisfies(rhs, deps=False)]
+
+            # Check whether the node needs matching (not a conditional that isn't satisfied)
+            in_edges = [
+                e
+                for e in rhs.edges_from_dependents()
+                if e.parent.satisfies(e.when)
+                or (lhss and all(lhs.satisfies(e.when) for lhs in lhss))
+            ]
+            if not in_edges:
+                continue
+
+            # If there is no matching lhs for this rhs node
+            if not lhss:
+                return False
+
+        return True
 
     @property  # type: ignore[misc] # decorated prop not supported in mypy
     def patches(self):
